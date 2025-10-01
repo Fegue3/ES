95ad69b50703eeac57f9835ec934ef8236128b58
Francisco <fp226766@gmail.com>
2025-10-01 15:55:21 +0100

    added about page tsx and css file

---
+++ frontend/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,10 @@
+import './AboutPage.css'
+
+export function AboutPage() {
+  return (
+    <section className="about">
+      <h1>About</h1>
+      <p>Página de exemplo.</p>
+    </section>
+  )
+}
